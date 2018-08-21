064e64a443026a96c9d1e5a5db458065bde795f2
zengsj <zengshuangjie@gmail.com>
2018-08-21 16:22:07 +0800

    update

--- Vuem.js
+++ Vuem.js
@@ -1,5 +1,46 @@
 let Vue;
 
+function checkVueVersion(requiredVue) {
+  const vueDep = requiredVue || '^2.5.10';
+  const required = vueDep.split('.', 3).map((v) => Number.parseInt(v.replace(/\D/g, ''), 10));
+  const actual = Vue.version.split('.', 3).map((n) => Number.parseInt(n, 10));
+  // Simple semver caret range comparison
+  const passes = actual[0] === required[0] && ( // major matches
+    // minor is greater
+    (actual[1] > required[1])
+    // or minor is eq and patch is >=
+    || (actual[1] === required[1] && actual[2] >= required[2])
+  );
+
+  assert(passes, `Vuem requires Vue version ${vueDep}.`);
+}
+
+function assert(condition, msg) {
+  if (!condition) throw new Error(`[Vuem] ${msg}`);
+}
+
+const bailRE = /[^\w.$]/;
+
+function parsePath(path) {
+  if (bailRE.test(path)) {
+    return;
+  }
+  const segments = path.split('.');
+
+  return function(obj) {
+    for (let i = 0; i < segments.length; i++) {
+      if (!obj) return;
+      obj = obj[`$${segments[i]}`];
+    }
+    
+    return obj;
+  };
+}
+
+function isVuem(inst) {
+  return inst instanceof Vue && inst._isVuem;
+}
+
 class Vuem {
   static install(_Vue) {
     if (Vue && _Vue === Vue) return;
@@ -8,20 +49,80 @@ class Vuem {
 
     checkVueVersion();
 
+    Vue.prototype.$prevent = function(prevented) {
+      this._prevented = Boolean(prevented);
+    };
+
+    const originEmit = Vue.prototype.$emit;
+
+    Vue.prototype.$emit = function(...args) {
+      if (this._prevented) return this;
+
+      return originEmit.apply(this, args);
+    };
+
     Vue.mixin({ beforeCreate: modelInit });
 
     function modelInit() {
-      const options = this.$options;
+      this.$rootModel = collectRootModel(this.$options);
+      this.$model = collectModel(this.$options) || this.$rootModel;
+    }
 
-      if (options.model) {
-        this.$model = typeof options.model === 'function'
-          ? options.model()
-          : options.model;
+    function collectRootModel(options) {
+      let rootModel = typeof options.model === 'function'
+        ? options.model()
+        : isVuem(options.model)
+          ? options.model
+          : null;
+
+      if (options.parent && options.parent.$rootModel) {
+        rootModel = options.parent.$rootModel;
+      }
+
+      return rootModel;
+    }
+
+    function collectModel(options) {
+      let model;
+
+      if (options.parent && options.parent.$rootModel) {
+        model = options.parent.$rootModel;
+      }
+
+      if (!options.model || isVuem(options.model)) return model;
+
+      if (typeof options.model === 'string') {
+        model = options.model === ''
+          ? model
+          : parsePath(options.model)(model);
       }
       else
-      if (options.parent && options.parent.$model) {
-        this.$model = options.parent.$model;
+      if (Array.isArray(options.model)) {
+        if (options.model.length) {
+          const wrapped = {};
+
+          options.model.forEach((name) => {
+            wrapped[name] = parsePath(name)(model);
+          });
+
+          model = wrapped;
+        }
+        else {
+          model = this.$rootModel;
+        }
       }
+      else
+      if (typeof options.model === 'object') {
+        const wrapped = {};
+
+        Object.keys(options.model).forEach((name) => {
+          wrapped[name] = parsePath(options.model[name])(model);
+        });
+
+        model = wrapped;
+      }
+
+      return model;
     }
   }
 
@@ -40,7 +141,30 @@ class Vuem {
 function createModel(options = {}) {
   const opts = Object.assign(
     {
-      name      : 'vuem',
+      mixins : [ {
+        methods : {
+          $dispatch(name, ...args) {
+            function doAction() {
+              return this.$actions[name].apply(this, args);
+            }
+
+            const actions = [];
+
+            actions.push(doAction.call(this));
+
+            this.$children.forEach((child) => {
+              actions.push(doAction.call(child));
+            });
+
+            return Promise.all(actions);
+          },
+          $broadcast(event, ...args) {
+            this.$root.$emit(event, ...args);
+            
+            return this;
+          },
+        },
+      } ],
       computed  : {},
       models    : {},
       actions   : {},
@@ -63,33 +187,19 @@ function createModel(options = {}) {
     );
 
     modelProperty.get = () => (model);
-    Object.defineProperty(vuem, key, modelProperty);
+    Object.defineProperty(vuem, `$${key}`, modelProperty);
   });
 
-  Object.keys(opts.actions).forEach((key) => {
+  modelProperty.get = () => (opts.actions);
+  Object.defineProperty(vuem, '$actions', modelProperty);
 
+  Object.keys(opts.broadcast).forEach((key) => {
+    this.$root.$on(key, opts.broadcast[key].bind(vuem));
   });
+
+  vuem._isVuem = true;
 
   return vuem;
-}
-
-function checkVueVersion(requiredVue) {
-  const vueDep = requiredVue || '^2.5.10';
-  const required = vueDep.split('.', 3).map((v) => Number.parseInt(v.replace(/\D/g, ''), 10));
-  const actual = Vue.version.split('.', 3).map((n) => Number.parseInt(n, 10));
-  // Simple semver caret range comparison
-  const passes = actual[0] === required[0] && ( // major matches
-    // minor is greater
-    (actual[1] > required[1])
-    // or minor is eq and patch is >=
-    || (actual[1] === required[1] && actual[2] >= required[2])
-  );
-
-  assert(passes, `Vuem requires Vue version ${vueDep}.`);
-}
-
-function assert(condition, msg) {
-  if (!condition) throw new Error(`[Vuem] ${msg}`);
 }
 
 export default Vuem;
